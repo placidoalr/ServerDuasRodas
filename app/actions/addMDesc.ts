500f7a780dbff9fb3fc65916b7d3909ef6bb4f0c
placidoalr <40044062+placidoalr@users.noreply.github.com>
2019-06-03 19:52:44 -0300

    DESC

---
+++ app/actions/addMDesc.ts
@@ -0,0 +1,52 @@
+import {Post} from '../decorators';
+import {Action} from '../kernel/action';
+import {ActionType} from '../kernel/route-types';
+import {VPUtils} from '../utils/vputils';
+import {KernelUtils} from '../kernel/kernel-utils';
+import { MySQLFactory } from '../mysql/mysql_factory';
+
+export class AddMDescAction extends Action{
+
+    private validateData(){
+        new KernelUtils().createExceptionApiError('1001', 'Informe o número do cracha, usuário e senha', this.req.body.userId == '' || this.req.body.userName == '' || this.req.body.password == '');
+    }
+
+    private generateSQL() : string {
+        return 'select * from TBUSUARIO where TBUSUARIO.LOGIN = \'' + this.req.body.userName + '\';';
+    }
+    private insertUserSQL() : string{
+        return 'insert into TBUSUARIO (TBUSUARIO.CDUSUARIO ,TBUSUARIO.LOGIN, TBUSUARIO.SENHA, TBUSUARIO.CDPERMISSAO) values (\''+ this.req.body.userId+'\',\''+ this.req.body.userName +'\', \''+ this.req.body.password+'\', \''+ this.req.body.permissao+'\');';
+    }
+
+    @Post('/addUser')
+    public Post(){
+        this.validateData();
+
+        new MySQLFactory().getConnection().select(this.generateSQL()).subscribe(
+            (data : any) => {
+                if (data.length || data.length > 0){
+                    console.log(data);
+                  this.sendError(new KernelUtils().createErrorApiObject(401, '1001', 'Usuário já existe'));
+                  return;
+                }else{
+                    console.log(data);
+                    new MySQLFactory().getConnection().select(this.insertUserSQL()).subscribe(
+                        (data : any) => {
+                            console.log(data);
+                        }
+                    );
+                }
+                this.sendAnswer({
+                    token    : new VPUtils().generateGUID().toUpperCase()
+                });
+            },
+            (error : any) => {
+                this.sendError(error);
+            }
+        );
+    }
+
+    defineVisibility() {
+        this.actionEscope = ActionType.atPublic;
+    }
+}
--- app/actions/index.ts
+++ app/actions/index.ts
@@ -2,8 +2,10 @@ import {LogonAction} from './logon';
 import {AddUserAction} from './addUser';
 import {ListaOMAction} from './listaOM';
 import {OMAction} from './om';
+import {AddMDescAction} from './addMDesc';
 
 export {AddUserAction};
 export {LogonAction};
 export {ListaOMAction};
-export {OMAction}
+export {OMAction};
+export {AddMDescAction};
