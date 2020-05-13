1958839f28e12f48faf58beb124b8e8b2e152d43
PLACIDO ANTONIO LANGER REINERT <40044062+placidoalr@users.noreply.github.com>
2020-05-13 19:42:32 -0300

    Select Manutentores
    /GetManuts = idUser

---
+++ app/actions/selectManuts.ts
@@ -0,0 +1,38 @@
+import {Get, Patch, Post, Put} from '../decorators';
+import {Action} from '../kernel/action';
+import {ActionType} from '../kernel/route-types';
+import {VPUtils} from '../utils/vputils';
+import {KernelUtils} from '../kernel/kernel-utils';
+import {MySQL} from '../mysql/mysql';
+import {MySQLFactory} from '../mysql/mysql_factory';
+
+export class selectManutsAction extends Action{
+
+
+    private selectSQL() : string {
+        return 'select ID,NOME from TBUSUARIO where TBUSUARIO.ID != \'' + this.req.body.idUser + '\' and TBUSUARIO.CARGO = 1;';
+    }
+    
+    @Get('/GetManuts')
+    public Get(){
+        
+        new MySQLFactory().getConnection().select(this.selectSQL()).subscribe(
+            (data : any) => {
+                this.sendAnswer(data);
+            },
+            (error : any) => {
+                this.sendError(error);
+            }
+        );
+    }
+    
+    defineVisibility() {
+        this.actionEscope = ActionType.atPublic;
+    }
+}
+
+
+
+
+
+
