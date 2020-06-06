baa9f54b087af2df276cf773e6dd9eba85fe4f67
PLACIDO ANTONIO LANGER REINERT <40044062+placidoalr@users.noreply.github.com>
2020-06-06 18:46:59 -0300

    a

--- app/actions/om.ts
+++ app/actions/om.ts
@@ -20,7 +20,7 @@ export class OMAction extends Action{
         return 'insert into TBOM (IDSAP,SOLIC,IDLAYOUT,IDCT,TPOM,CAUSADEF,DEF,DTGERACAO,OBS,PRIORIDADE,ESTADO,LOC_INST_ATRIB,REQUERPARADA,DT_INI_PLAN,DT_INI_PROG,DT_FIM_PLAN,DT_FIM_PROG ) values (\''+ this.req.body.idsap+'\',\''+ this.req.body.solicitante+'\','+ this.req.body.layout+','+ this.req.body.ct+','+ this.req.body.tipoManut+','+ this.req.body.causa+',\''+ this.req.body.def+'\',NOW(), \''+ this.req.body.obs+'\','+this.req.body.prior+', 1,'+this.req.body.li+',\''+this.req.body.requerParada+'\',\''+this.req.body.dtIniPlan+'\',\''+this.req.body.dtIniProg+'\',\''+this.req.body.dtFimPlan+'\',\''+this.req.body.dtFimProg+'\');';
     }
     private insertEQUIPSQL(equip : any, id : any){
-        return 'insert into TBEQUIP_WITH_TBOM (IDOM,IDEQUIP, OPER,MAT_UTIL,QTDE_MAT) values ('+ id+','+ equip.id+',\''+ equip.oper+'\','+ equip.material+',\''+ equip.qtde+'\');';
+        return 'insert into TBEQUIP_WITH_TBOM (IDOM,IDEQUIP, OPER,MAT_UTIL,QTDE_MAT) values ('+ id+','+ equip.id+',\''+ equip.oper+'\','+ equip.material+','+ equip.qtde+');';
     }
     private insertMATSQL(mat : any, id : any){
         return 'insert into TBMAT_WITH_OM (IDOM, IDMAT, QTDE) values ('+ id+','+ mat.id+','+ mat.qtde+');';
